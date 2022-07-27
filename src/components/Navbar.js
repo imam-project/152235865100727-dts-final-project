bcaa110724984efeb3d2c3290cc8dc0d25da08d9
Bagas Imam Mahardika Wijaya <bagasimam.mw@gmail.com>
2022-07-28 01:42:07 +0700

    login-register

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,12 +6,14 @@ import Logo from "../assets/logo.png";
 import UserLog from "./UserLog";
 
 function Navbar() {
+	
 	return (
 		<Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: "122px", xs: "40px" }, alignItems: "center", width:'100%', height: '70px'}} px="20px" backgroundColor="#30475E">
 			<Link to="/">
 				<img src={Logo} alt="logo" style={{ width: "48px", height: "48px", margin: "0 20px" }} />
 			</Link>
-			<Stack direction="row" gap="40px" fontSize="20px" alignItems="flex-end">
+			<Stack direction="row" gap="40px" fontSize="20px"
+			alignItems="flex-end">
 				<Link to="/" style={{ textDecoration: "none", color: "#ffff", borderBottom: "3px solid #F05454" }}>
 					Home
 				</Link>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,15 @@
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import { Paper, Stack } from "@mui/material";
+import { Paper } from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import * as React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import bg from '../assets/bg.jpg'
+import bg from "../assets/bg.jpg";
 
 import { auth } from "../auth/firebase";
 
@@ -31,53 +30,54 @@ const Login = () => {
 			setErrorMessage(error.message);
 		}
 	};
-    const styles = {
-        paperContainer: {
-            backgroundImage: `url(${bg})`
-        }
-    };
+	const styles = {
+		paperContainer: {
+			backgroundImage: `url(${bg})`,
+		},
+	};
 
 	return (
-		<Paper style={styles.paperContainer} sx={{justifyContent: 'center', alignItems:'center'}}>
+		<Paper style={styles.paperContainer} sx={{ justifyContent: "center", alignItems: "center" }}>
 			<Box
 				sx={{
 					display: "flex",
 					flexDirection: "column",
 					alignItems: "center",
-                    justifyContent: "center",
-                    height: "1080px"
-
+					justifyContent: "center",
+					height: "1080px",
 				}}
 			>
-                <Box sx={{
-                    display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
-                    justifyContent: "center",
-                    backgroundColor: '#30475E',
-                    borderRadius: '15px',
-                    height: "500px",
-                    width: "700px"
-                }}>
-				<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-					<LockOutlinedIcon />
-				</Avatar>
-				<Typography component="h1" variant="h5">
-					Sign in
-				</Typography>
-				<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-					<TextField margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus sx={{ backgroundColor: "#ffff" }} />
-					<TextField margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" sx={{ backgroundColor: "#ffff" }} />
-					<Typography color="red">{errorMessage}</Typography>
-					<Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-						Sign In
-					</Button>
-					<Grid container>
-						<Grid item>
-							<Link to="/register">Don't have an account? Sign Up"</Link>
+				<Box
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+						justifyContent: "center",
+						backgroundColor: "#30475E",
+						borderRadius: "15px",
+						height: "500px",
+						width: "700px",
+					}}
+				>
+					<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component="h1" variant="h5">
+						Sign in
+					</Typography>
+					<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+						<TextField margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus sx={{ backgroundColor: "#ffff" }} />
+						<TextField margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" sx={{ backgroundColor: "#ffff" }} />
+						<Typography color="red">{errorMessage}</Typography>
+						<Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+							Sign In
+						</Button>
+						<Grid container>
+							<Grid item>
+								<Link to="/register">Don't have an account? Sign Up"</Link>
+							</Grid>
 						</Grid>
-					</Grid>
-                </Box>
+					</Box>
 				</Box>
 			</Box>
 		</Paper>
